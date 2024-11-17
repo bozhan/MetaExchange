313518b6911b15b571f7041918c625e0a11589b5
Bozhan Ivanov <bozhan.ivanov@adesso.de>
2024-11-17 20:00:30 +0100

    feat(models): add Order model representing individual buy/sell orders

---
+++ MetaExchange.ConsoleApp/Models/Order.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace MetaExchange.ConsoleApp.Models
+{
+    public class Order
+    {
+        [JsonPropertyName("Id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("Time")]
+        public DateTime Time { get; set; }
+
+        [JsonPropertyName("Type")]
+        public string Type { get; set; } // "Buy" or "Sell"
+
+        [JsonPropertyName("Kind")]
+        public string Kind { get; set; } // "Limit"
+
+        [JsonPropertyName("Amount")]
+        public double Amount { get; set; }
+
+        [JsonPropertyName("Price")]
+        public double Price { get; set; }
+    }
+}

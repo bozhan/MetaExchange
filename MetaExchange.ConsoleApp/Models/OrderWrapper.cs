53efe30b5860e9bea212f73bc1623b96de9d3abc
Bozhan Ivanov <bozhan.ivanov@adesso.de>
2024-11-17 20:03:02 +0100

    feat(models): add OrderWrapper model to encapsulate Order objects within OrderBook

---
+++ MetaExchange.ConsoleApp/Models/OrderWrapper.cs
@@ -0,0 +1,10 @@
+using System.Text.Json.Serialization;
+
+namespace MetaExchange.ConsoleApp.Models
+{
+    public class OrderWrapper
+    {
+        [JsonPropertyName("Order")]
+        public Order? Order { get; set; }
+    }
+}

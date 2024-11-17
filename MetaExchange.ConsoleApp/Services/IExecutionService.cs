8a4bf7331cb1b0ead9d5c921b16e6950e0c5412a
Bozhan Ivanov <bozhan.ivanov@adesso.de>
2024-11-17 20:18:20 +0100

    feat(services): define IExecutionService interface for computing execution plans

---
+++ MetaExchange.ConsoleApp/Services/IExecutionService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MetaExchange.ConsoleApp.Models;
+
+namespace MetaExchange.ConsoleApp.Services
+{
+    public interface IExecutionService
+    {
+        Task<ExecutionPlan> GetBestExecutionPlanAsync(List<Exchange> exchanges, string orderType, double amount);
+    }
+}

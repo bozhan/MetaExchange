491217c11364f206aaaee50f041df6adc388a6d0
Bozhan Ivanov <bozhan.ivanov@adesso.de>
2024-11-18 02:17:45 +0100

    chore: updated program.cs to include registration of swagger and controllers

--- MetaExchange.WebApi/Program.cs
+++ MetaExchange.WebApi/Program.cs
@@ -17,15 +17,34 @@ builder.Services.AddSingleton<IExecutionService, ExecutionService>();
 // Configure logging
 builder.Services.AddLogging();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Bind configuration
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
 // Build the app
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+else
+{
+	// In production, you might want to serve Swagger UI at a different endpoint or disable it
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
 app.UseRouting();
 
-app.UseEndpoints(endpoints =>
-{
-	endpoints.MapControllers();
-});
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
